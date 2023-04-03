20c5bc2b1f08bbed020b30ca0739fa676c3e1b51
KorolevskiyK <K-korolevskiy@mail.ru>
2023-04-03 20:41:37 +0300

    add plugin

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    })
+function slidesPlugin(activeSlide = 3) {
+    slides[activeSlide].classList.add('active');
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-}
+slidesPlugin();
